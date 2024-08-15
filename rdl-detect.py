9e6febda1a8e642c32e39acf5a925ab3ca0e97ac
Ankio <ankio@ankio.net>
2024-08-15 09:26:49 +0800

    feat: 支持批量探测

--- rdl-detect.py
+++ rdl-detect.py
@@ -32,16 +32,16 @@ class RPCDump:
         443: {'bindstr': r'ncacn_http:[593,RpcProxy=%s:443]'},
         445: {'bindstr': r'ncacn_np:%s[\pipe\epmapper]'},
         593: {'bindstr': r'ncacn_http:%s'}
-        }
+    }
 
-    def __init__(self, username = '', password = '', domain='', hashes = None, port=135):
+    def __init__(self, username='', password='', domain='', hashes=None, port=135):
         self.__username = username
         self.__password = password
         self.__domain = domain
         self.__lmhash = ''
         self.__nthash = ''
         self.__port = port
-        self.__stringbinding = '' 
+        self.__stringbinding = ''
         if hashes is not None:
             self.__lmhash, self.__nthash = hashes.split(':')
 
@@ -69,22 +69,13 @@ class RPCDump:
             rpctransport.set_credentials(self.__username, self.__password, self.__domain,
                                          self.__lmhash, self.__nthash)
 
-            # Usually when a server doesn't support NTLM, it also doesn't expose epmapper (nowadays
-            # only RDG servers may potentially expose a epmapper via RPC Proxy).
-            #
-            # Also if the auth is not NTLM, there is no way to get a target
-            # NetBIOS name, but epmapper ACL requires you to specify it.
             rpctransport.set_auth_type(AUTH_NTLM)
         else:
-            # We don't need to authenticate to 135 and 593 ports
             pass
 
         try:
             entries = self.__fetchList(rpctransport)
         except Exception as e:
-            #raise
-
-            # This may contain UTF-8
             error_text = 'Protocol failed: %s' % e
             logging.critical(error_text)
 
@@ -96,16 +87,12 @@ class RPCDump:
                                  "to connect to its epmapper using RpcProxy.")
                 return
 
-        # Display results.
-
         endpoints = {}
-        # Let's groups the UUIDS
         for entry in entries:
             binding = epm.PrintStringBinding(entry['tower']['Floors'])
             tmpUUID = str(entry['tower']['Floors'][0])
-            if (tmpUUID in endpoints) is not True:
-                endpoints[tmpUUID] = {}
-                endpoints[tmpUUID]['Bindings'] = list()
+            if tmpUUID not in endpoints:
+                endpoints[tmpUUID] = {'Bindings': []}
             if uuid.uuidtup_to_bin(uuid.string_to_uuidtup(tmpUUID))[:18] in epm.KNOWN_UUIDS:
                 endpoints[tmpUUID]['EXE'] = epm.KNOWN_UUIDS[uuid.uuidtup_to_bin(uuid.string_to_uuidtup(tmpUUID))[:18]]
             else:
@@ -117,27 +104,33 @@ class RPCDump:
                 endpoints[tmpUUID]['Protocol'] = epm.KNOWN_PROTOCOLS[tmpUUID[:36]]
             else:
                 endpoints[tmpUUID]['Protocol'] = "N/A"
-            #print("Transfer Syntax: %s" % entry['tower']['Floors'][1])
 
         found = False
+      # ANSI escape sequences for colors
+        RED = '\033[91m'
+        GREEN = '\033[92m'
+        YELLOW = '\033[93m'
+        CYAN = '\033[96m'
+        RESET = '\033[0m'
+
         for endpoint in list(endpoints.keys()):
             if '3d267954-eeb7-11d1-b94e-00c04fa3080d' in endpoint.lower():
-                logging.info('RPC探测发现 %s 主机开放Terminal Server Licensing服务，可能受影响' % remoteName)
-                print('服务信息：')
-                print("Protocol: %s " % endpoints[endpoint]['Protocol'])
-                print("Provider: %s " % endpoints[endpoint]['EXE'])
-                print("UUID    : %s %s" % (endpoint, endpoints[endpoint]['annotation']))
-                print("Bindings: ")
+                logging.info(GREEN + ' %s Found Terminal Server Licensing ' % remoteName + RESET)
+                logging.debug(YELLOW + '服务信息：' + RESET)
+                logging.debug(GREEN + "Protocol: %s " % endpoints[endpoint]['Protocol'] + RESET)
+                logging.debug(GREEN + "Provider: %s " % endpoints[endpoint]['EXE'] + RESET)
+                logging.debug(GREEN + "UUID    : %s %s" % (endpoint, endpoints[endpoint]['annotation']) + RESET)
+                logging.debug(GREEN + "Bindings: " + RESET)
                 found = True
                 for binding in endpoints[endpoint]['Bindings']:
-                    print("          %s" % binding)
+                    logging.debug(CYAN + "          %s" % binding + RESET)
                 break
-        
+
         if not entries:
-            logging.info('%s 主机未开放RPC服务，探测失败' % remoteName)
+            logging.critical(YELLOW + ' %s RPC Check Error ' % remoteName + RESET)
 
         if entries and not found:
-            logging.info('RPC探测发现 %s 主机未开放Terminal Server Licensing服务，不受影响' % remoteName)
+            logging.critical(RED + ' %s Not Found Terminal Server Licensing' % remoteName + RESET)
 
 
     def __fetchList(self, rpctransport):
@@ -153,50 +146,31 @@ class RPCDump:
 
 # Process command-line arguments.
 if __name__ == '__main__':
-    # Init the example's logger theme
     logger.init()
 
-    parser = argparse.ArgumentParser(add_help = True, description = "基于RPC dump排查RDL服务开放情况")
-    parser.add_argument('target', action='store', help='[[domain/]username[:password]@]<targetName or address>')
-    parser.add_argument('-debug', action='store_true', help='Turn DEBUG output ON')
+    parser = argparse.ArgumentParser(add_help=True, description="基于RPC dump排查RDL服务开放情况")
+    parser.add_argument('-f', '--file', required=True, help='包含目标IP的文件，每行一个IP地址或主机名')
+    parser.add_argument('-debug', action='store_true', help='调试模式')
 
-    group = parser.add_argument_group('connection')
-
-    group.add_argument('-target-ip', action='store', metavar="ip address", help='IP Address of the target machine. If '
-                       'ommited it will use whatever was specified as target. This is useful when target is the NetBIOS '
-                       'name and you cannot resolve it')
-    group.add_argument('-port', choices=['135', '139', '443', '445', '593'], nargs='?', default='135', metavar="destination port",
-                       help='Destination port to connect to RPC Endpoint Mapper')
-
-    group = parser.add_argument_group('authentication')
-
-    group.add_argument('-hashes', action="store", metavar = "LMHASH:NTHASH", help='NTLM hashes, format is LMHASH:NTHASH')
-    if len(sys.argv)==1:
+    parser.add_argument('-port', choices=['135', '139', '443', '445', '593'], nargs='?', default='135', metavar="destination port",
+                       help='需要探测的端口，默认为135')
+    if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
- 
+
     options = parser.parse_args()
 
     if options.debug is True:
         logging.getLogger().setLevel(logging.DEBUG)
-        # Print the Library's installation path
         logging.debug(version.getInstallationPath())
     else:
         logging.getLogger().setLevel(logging.INFO)
 
-    domain, username, password, remoteName = parse_target(options.target)
+    with open(options.file, 'r') as file:
+        targets = file.read().splitlines()
 
-    if domain is None:
-        domain = ''
+    for target in targets:
 
-    if password == '' and username != '' and options.hashes is None:
-        from getpass import getpass
-        password = getpass("Password:")
+        dumper = RPCDump('', '', target, ':', int(options.port))
 
-    if options.target_ip is None:
-        options.target_ip = remoteName
-
-    dumper = RPCDump(username, password, domain, options.hashes, int(options.port))
-
-    dumper.dump(remoteName, options.target_ip)
-
+        dumper.dump(target, target)
